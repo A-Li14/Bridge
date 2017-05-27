d0989be4fd8247a00197ad07fcd1ac08c4f3910c
A-Li14 <Alexander.Li.473@gmail.com>
2017-05-27 17:45:34 -0400

    add images to git

---
+++ Cards Application/Clean_Images.R
@@ -0,0 +1,16 @@
+###Gather all card image files into a data.table for easy loading
+require(data.table)
+files = list.files(pattern="png|PNG",recursive=T)
+files = files[grep("www",files,invert=T)]
+files = files[grep("joker",files,invert = T)]
+royals = files[grep("2.png",files)]
+royals = royals[grep("ace",royals,invert=T)]
+
+files = files[grep("king|jack|queen|spades2",files,invert=T)]
+files = c(files,royals)
+
+###Alternative - 
+files = gsub("/","",str_extract(files,"/.{10,20}\\.png"))
+
+image_paths = paste('<img src="',files,'" height = "100" width = "72"></img>',sep="")
+saveRDS(image_paths,"Cards Application/data/image_paths.RDS")
---
+++ Cards Application/Complete Deck.R
@@ -0,0 +1,23 @@
+###Create combined file for card definitions and images
+require(data.table)
+require(stringr)
+
+deck = readRDS("Cards Application/data/deck.RDS")
+image_paths = readRDS("Cards Application/data/image_paths.RDS")
+
+image_dt = data.table(path=image_paths)
+image_dt[,suit:="Spades"]
+image_dt[grep("heart",path),suit:="Hearts"]
+image_dt[grep("clubs",path),suit:="Clovers"]
+image_dt[grep("diamonds",path),suit:="Diamonds"]
+
+image_dt[,number:=toupper(str_extract(str_extract(path,"[0-9a-z]{1,5}_of"),"[0-9]{1,2}|[a-z]"))]
+image_dt
+deck = deck[image_dt,on=.(number,suit)]
+deck[,number:=factor(number,levels = c(2:10,"J","Q","K","A"))]
+deck[,suit:=factor(suit,levels=c("Diamonds","Clovers","Hearts","Spades"))]
+setkey(deck,suit,number)
+saveRDS(deck,"Cards Application/data/deck2.RDS")
+
+
+
